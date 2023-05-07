f4d165c7384aade71d828fb08502d8ceda7dca89
Maximilian Øystå Lloyd <me@maxlloyd.no>
2023-05-07 21:56:46 +0200

    Added movie category and starwars subcategory

--- lua/ascii/art.lua
+++ lua/ascii/art.lua
@@ -3,13 +3,15 @@ local text = require("ascii.text")
 local misc = require("ascii.misc")
 local gaming = require("ascii.gaming")
 local planets = require("ascii.planets")
+local movies = require("ascii.movies")
 
 local M = {
 	animals = animals,
 	text = text,
 	misc = misc,
 	gaming = gaming,
-	planets = planets
+	planets = planets,
+	movies = movies
 }
 
 return M
---
+++ lua/ascii/movies/init.lua
@@ -0,0 +1,7 @@
+local starwars = require("ascii.movies.starwars")
+
+local M = {
+	starwars = starwars,
+}
+
+return M
