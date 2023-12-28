d1824629f841cde3f9c717fc64b99c8f18503624
Prashant <prashantrahul141@protonmail.com>
2023-12-28 20:23:51 +0530

    add: sunset ascii arts

--- lua/ascii/misc/init.lua
+++ lua/ascii/misc/init.lua
@@ -1,11 +1,13 @@
 local skulls = require("ascii.misc.skulls")
 local krakens = require("ascii.misc.krakens")
 local hydra = require("ascii.misc.hydra")
+local sunset = require("ascii.misc.sunset")
 
 local M = {
 	skulls = skulls,
 	krakens = krakens,
 	hydra = hydra,
+	sunset = sunset,
 }
 
 return M
